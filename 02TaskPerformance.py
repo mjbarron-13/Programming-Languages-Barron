99b9731192f3805b9afe01a91639b78527742eb8
mjbarron-13 <matteojb134@gmail.com>
2025-02-14 10:01:44 +0000

    02 Task Performance Submission

---
+++ 02TaskPerformance.py
@@ -0,0 +1,34 @@
+#1.Initial Invocation
+print("I never thought through love we'd be")
+print("Making one as lovely as she")
+
+#2.Adding an Empty print() function
+print("I never thought through love we'd be")
+print()
+print("Making one as lovely as she")
+
+#3.Inserting \n in between the arguments
+print("I never thought through love we'd be\nMaking one as lovely as she")
+
+
+#4.Separate the first print() function into three arguments
+print("I never thought", "through love", "we'd be")
+print("Making one as lovely as she")
+
+#5.Use end=" " in the first print()
+print("I never thought", "through love", "we'd be", end=" ")
+print("Making one as lovely as she") 
+
+#6.Use sep="-" to separate words with dashes instead of spaces
+print("I never thought", "through love", "we'd be", end=" ")
+print("Making", "one", "as", "lovely", "as", "she", sep="-")
+
+#7.Use both end=" " and sep="-" in both print() functions
+print("I", "never", "thought", "through", "love", "we'd", "be", sep="-", end=" ")
+print("Making", "one", "as", "lovely", "as", "she", sep="-")
+
+
+
+
+
+
