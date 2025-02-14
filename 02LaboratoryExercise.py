a3f8e76cba9501dad61e4067f37b8f6436bf9491
mjbarron-13 <matteojb134@gmail.com>
2025-02-14 17:32:58 +0800

    Submission for 02 Laboratory Exercise

---
+++ 02LaboratoryExercise.py
@@ -0,0 +1,19 @@
+# Initial Invocation
+print("hello world")
+
+# Changing to First Name
+print("Matteo")
+
+#Without Double Quotes
+#NameError
+#print(Matteo)
+
+#Without Parentheses
+#SyntaxError
+#print "Matteo"
+
+#wrong spelling of syntax
+#NameError
+#prnt("Matteo")
+
+
