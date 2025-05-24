a4e855d7c9fa2aa6efa7333fb74e170fe7fbfbe3
Matteo Juro Barron <matteojb134@gmail.com>
2025-05-24 18:43:03 +0800

    Create 07LabTaskCBarron.py

---
+++ 07LabTaskCBarron.py
@@ -0,0 +1,8 @@
+# Task C - Password Retry Loop
+correct_password = "default123"
+user_input = ""
+
+while user_input != correct_password:
+    user_input = input("Enter password: ")
+
+print("Access granted!")
